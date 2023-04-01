2ef95ac7c057ba2dbea51d5b19448c1878317cee
CS20B019-Manasa <cs20b019@iittp.ac.in>
2023-04-01 10:53:43 +0530

    calculated energy consumed and carbon footprint for sql query

--- database/time_consumption.py
+++ database/time_consumption.py
@@ -3,10 +3,13 @@ from pymongo import MongoClient
 import mysql.connector
 import psutil
 import time
+import sys
+sys.path.insert(0, "./")
+from main import Tracker           # Tracker where all metric calculation functions are implemented
 
 def execute_query(query, database_type):
     if database_type == "SQL":
-        connection = mysql.connector.connect(user='root', password='password_of_mysql', host='localhost', database='database_name')
+        connection = mysql.connector.connect(user='root', password='', host='localhost', database='cs20b019')
         cursor = connection.cursor()
         cursor.execute(query)
         if query[0] == "I" or query[0] == "D" or query[0] == "U":
@@ -18,16 +21,25 @@ def execute_query(query, database_type):
             return result_set
     elif database_type == "NoSQL":
         client = MongoClient('mongodb://localhost:27017/')
-        db = client['database_name']
+        db = client['cs20b019']
         client.close()
 
 def calculate_time_consumption(start_time, end_time):
     time_consumption = end_time - start_time
     return time_consumption
 
-start_time = time.time()
-query = "UPDATE department SET dname = 'Manasa1' WHERE dnumber = 1234;"
+# start_time = time.time()
+obj = Tracker()
+obj.start()
+        
+query = "UPDATE department SET dname = 'Manasa3' WHERE dnumber = 1234;"
 res=execute_query(query, "SQL")
-end_time = time.time()
-time_consumed = calculate_time_consumption(start_time, end_time)
-print("Time consumption:", time_consumed)
+# end_time = time.time()
+obj.stop()
+# time_consumed = calculate_time_consumption(start_time, end_time)
+print("CPU Consumption: ",obj.cpu_consumption())
+print("RAM Consumption: ",obj.ram_consumption())
+print("Total Consumption: ",obj.consumption())
+print("CO2 Emmissions: ",obj._construct_attributes_dict()['CO2_emissions(kg)'][0])
+
+# print("Time consumption:", time_consumed)
